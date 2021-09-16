87013e5272f4c7aa178c29b7a1789bcdcb01cc41
devludaley <daleyluis@gmail.com>
2021-09-16 17:09:33 -0400

    connect db to node

--- .history/index_20210916170801.js
+++ .history/index_20210916170801.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require
 ('body-parser')
-const app = express()
 const db = require('./queries')
+
+const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
@@ -14,13 +15,6 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
